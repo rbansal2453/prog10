8d86c0618b60cb372fbd41f01878fd42a6e4d35c
Rajesh <Rajesh@RAJESH-PC>
2017-02-20 23:54:01 -0600

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary
+{
+    class login
+    {
+        public string check(string name, int pass)
+        {
+            return name == "rajesh" && pass == 123 ? "Welcome rajesh" : "Invalid Login or Password";
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            login log = new login();
+            Console.WriteLine(log.check("rajesh", 123));
+
+            Console.ReadLine();
+        }
+    }
+}
